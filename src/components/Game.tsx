775c9cc0fa1dbc329e0dbb1c9dcab01d5f6b06c1
halafi <filiphalas74@gmail.com>
2020-12-19 12:14:05 +0100

    feat: scaling, making moves for both players

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/no-array-index-key */
-import React from 'react';
+import React, { useState } from 'react';
 import Chess from 'chess.js';
+import classNames from 'classnames';
 
 import Bishop from './pieces/Bishop';
 import King from './pieces/King';
@@ -9,12 +10,12 @@ import Pawn from './pieces/Pawn';
 import Queen from './pieces/Queen';
 import Rook from './pieces/Rook';
 
+import './Game.css';
+
 type Square = {
   type: 'r' | 'n' | 'b' | 'k' | 'q' | 'p';
   color: 'b' | 'w';
 };
-
-const SIZE = 46;
 
 const lightSquareColor = 'bg-yellow-100';
 const darkSquareColor = 'bg-yellow-600';
@@ -30,50 +31,107 @@ const getSquareColor = (x: number, y: number) => {
   return odd ? darkSquareColor : lightSquareColor;
 };
 
-const squareToPiece = (square: Square | null) => {
+const squareToPiece = (square: Square | null, possibleMove: boolean, size: number) => {
   if (square) {
     const color = square.color === 'b' ? 'black' : 'white';
     if (square.type === 'p') {
-      return <Pawn variant={color} size={SIZE} />;
+      return <Pawn variant={color} size={size} />;
     }
     if (square.type === 'n') {
-      return <Knight variant={color} size={SIZE} />;
+      return <Knight variant={color} size={size} />;
     }
     if (square.type === 'b') {
-      return <Bishop variant={color} size={SIZE} />;
+      return <Bishop variant={color} size={size} />;
     }
     if (square.type === 'r') {
-      return <Rook variant={color} size={SIZE} />;
+      return <Rook variant={color} size={size} />;
     }
     if (square.type === 'q') {
-      return <Queen variant={color} size={SIZE} />;
+      return <Queen variant={color} size={size} />;
     }
     if (square.type === 'k') {
-      return <King variant={color} size={SIZE} />;
+      return <King variant={color} size={size} />;
     }
   }
-  return '';
+  // if (possibleMove) {
+  //   return <Move size={size} />;
+  // }
+  // placeholder
+  return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45 45" width={size} height="100%" />;
 };
 
-const Game = () => {
+const getSquare = (x: number, y: number): string => {
+  let letter = 'a';
+  if (x === 1) letter = 'b';
+  if (x === 2) letter = 'c';
+  if (x === 3) letter = 'd';
+  if (x === 4) letter = 'e';
+  if (x === 5) letter = 'f';
+  if (x === 6) letter = 'g';
+  if (x === 7) letter = 'h';
+  return `${letter}${8 - y}`;
+};
+
+const squareInMoves = (moves: string[], square: string): boolean => {
+  const move = moves.find((m) => m.includes(square));
+  if (move) return true;
+  return false;
+};
+
+type Props = {
+  tileSize?: number;
+};
+
+const Game = ({ tileSize }: Props) => {
   // @ts-ignore
-  const chess = new Chess();
+  const [chess, setChess] = useState(new Chess());
+  const [activeSquare, setActiveSquare] = useState<string | null>(null);
+  // force update hack
+  const [, updateState] = React.useState<any>();
+  const forceUpdate = React.useCallback(() => updateState({}), []);
+
   const board = chess.board();
-  console.log(board);
+  const moves = chess.moves({ square: activeSquare });
+
+  const makeMove = (to: string) => {
+    chess.move({ from: activeSquare, to });
+    forceUpdate();
+  };
 
   return (
-    <div className="flex flex-col w-96 border-2 border-gray-900">
+    <div className="flex flex-col border-2 border-gray-900 relative">
       {board.map((row: Square[], y: number) => (
         <div key={y} className="flex">
-          {row.map((square, x) => (
-            <div key={x} className={`w-full flex justify-center h-12 ${getSquareColor(x, y)}`}>
-              {squareToPiece(square)}
-            </div>
-          ))}
+          {row.map((square, x) => {
+            const sq: string = getSquare(x, y);
+            const isMove: boolean = squareInMoves(moves, sq);
+            return (
+              // eslint-disable-next-line jsx-a11y/click-events-have-key-events
+              <div
+                key={`${y}-${x}`}
+                className={`flex justify-center outline-none relative ${getSquareColor(x, y)}`}
+                onClick={() => (!isMove ? setActiveSquare(sq) : makeMove(sq))}
+                role="button"
+                tabIndex={0}
+              >
+                <div
+                  className={classNames('absolute top-0 left-0 w-full h-full', {
+                    'move-dest': isMove && !square,
+                    'move-take': isMove && square,
+                  })}
+                />
+                {squareToPiece(square, isMove, tileSize as number)}
+              </div>
+            );
+          })}
         </div>
       ))}
     </div>
   );
+};
+
+Game.defaultProps = {
+  tileSize: 46,
 };
 
 export default Game;
--- src/scenes/Root.tsx
+++ src/scenes/Root.tsx
@@ -2,52 +2,17 @@ import * as React from 'react';
 import useResizeAware from 'react-resize-aware';
 import Game from '../components/Game';
 
-// import './Root.css';
-
-const defaultLineup = [
-  'R@a1',
-  'P@a2',
-  'p@a7',
-  'r@a8',
-  'N@b1',
-  'P@b2',
-  'p@b7',
-  'n@b8',
-  'B@c1',
-  'P@c2',
-  'p@c7',
-  'b@c8',
-  'Q@d1',
-  'P@d2',
-  'p@d7',
-  'q@d8',
-  'K@e1',
-  'P@e2',
-  'p@e7',
-  'k@e8',
-  'B@f1',
-  'P@f2',
-  'p@f7',
-  'b@f8',
-  'N@g1',
-  'P@g2',
-  'p@g7',
-  'n@g8',
-  'R@h1',
-  'P@h2',
-  'p@h7',
-  'r@h8',
-];
-
 const Root = () => {
   const [resizeListener, sizes] = useResizeAware();
-  console.log(sizes);
 
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <h1>WIP</h1>
       {resizeListener}
-      <Game />
+      {sizes && sizes.width && sizes.height && (
+        <Game
+          tileSize={sizes.width > sizes.height ? (sizes.height - 20) / 8 : (sizes.width - 20) / 8}
+        />
+      )}
     </div>
   );
 };
